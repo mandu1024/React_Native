2245063bd989ea4e163251d52b82548e106c6dd7
mandu1024 <mandu.git@gmail.com>
2021-01-21 15:54:30 +0900

    react test

---
+++ CountApp/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Styled from 'styled-components/native';
+
+import Counter from './Screens/Counter'
+
+const Container =Styled.View`
+  flex: 1;
+  background-color: #EEE;
+`;
+
+
+export default function App() {
+  return (
+    <Container>
+      <Counter title="This is a Counter App" initValue={5} />
+    </Container>
+  );
+}
